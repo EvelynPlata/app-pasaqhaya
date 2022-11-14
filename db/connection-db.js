7e2c06cb6c2d01caa1b6b9a91fe10c90190a6d7a
cllanosucb <cllanos@ucb.edu.bo>
2022-11-14 18:17:00 -0400

    conexion con db

---
+++ db/connection-db.js
@@ -0,0 +1,39 @@
+const mysql = require('mysql2/promise');
+
+// Crear la conexión pool. configurando los datos de acceso a la DB
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    port: 3306,
+    database: 'prueba-mysql',
+});
+
+/*
+sql = sql de consulta: select, insert, update, delete
+values = es un array de datos
+*/
+db = async (sql, values) => {
+    try {
+        const respDB = await pool.query(sql, values);
+        return {
+            ok: true,
+            data: respDB[0]
+        }
+    } catch (error) {
+        console.log("ERROR", error);
+        return {
+            ok: false,
+            error: {
+                msg: 'Error al realizar la consulta a la base de datos',
+                code: error.code || null,
+                errno: error.errno || null,
+                sql: error.sql || null,
+                sqlState: error.sqlState || null,
+                sqlMessage: error.sqlMessage || null
+            }
+        }
+    }
+}
+
+module.exports = db;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+db = require('../db/connection-db');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function (req, res, next) {
+  const respDB = await db('SELECT * FROM productos', []);
+  /* lista = [];
+  const LONGITUD_PEDAZOS = 4; // Partir en arreglo de 4
+  for (let i = 0; i < respDB.data.length; i += LONGITUD_PEDAZOS) {
+    let pedazo = respDB.data.slice(i, i + LONGITUD_PEDAZOS);
+    lista.push(pedazo);
+  }
+  console.log(lista); */
+  res.render('index', { productos: respDB.data });
+});
+
+router.get('/categoria', async function (req, res, next) {
+  res.send('Categorías');
 });
 
 module.exports = router;
