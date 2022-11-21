c65983f4e917969d0146c0bbc72abae2891acabc
cllanosucb <cllanos@ucb.edu.bo>
2022-11-21 19:02:18 -0400

    Compras segunda parte

--- model/comprasModel.js
+++ model/comprasModel.js
@@ -2,7 +2,7 @@ db = require("../db/connection-db");
 
 carritoPorCliente = async (id_cliente) => {
     const sqlCarritoPorCliente = `SELECT p.id_pedido,p.id_cliente,p.total_pago,productos.id_producto,dp.cantidad_producto,dp.precio_producto_unitario,dp.precio_producto_total,
-    productos.nombre,productos.descripcion,productos.tipo,productos.precio,productos.imagen,productos.stock
+    productos.nombre,productos.descripcion,productos.tipo,productos.precio,productos.imagen,productos.stock,p.imagen_qr
     FROM pedidos AS p LEFT JOIN detalle_pedidos AS dp ON p.id_pedido = dp.id_pedido INNER JOIN productos ON dp.id_producto = productos.id_producto
     WHERE p.id_cliente = ? AND p.estado_pago = 0 AND p.estado_compra = 0 AND productos.estado = 1`;
     const dbCarrito = await db(sqlCarritoPorCliente, [id_cliente]);
